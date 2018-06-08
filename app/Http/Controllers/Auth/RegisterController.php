52eda8d5650cf48cdb6a12e631f837f5ab8de7be
Augustino Emanuel <augustino.emanuel73@gmail.com>
2018-06-08 09:40:03 +0000

    admin registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/superadmin';
 
     /**
      * Create a new controller instance.
