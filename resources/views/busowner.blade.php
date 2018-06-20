ca74ddc63f504ee0c8893e49a528bcdeb88391e9
defashi <davidfaustin74.df@gmail.com>
2018-06-20 14:30:35 +0300

    another changes

--- resources/views/busowner.blade.php
+++ resources/views/busowner.blade.php
@@ -13,7 +13,27 @@
                         </div>
                     @endif
 
-                    You are logged in as busowner
+                    <h3>Total Amount</h3>
+      <table border = "1">
+<tr style="text-align: center;">
+<td><b>Name</td>
+<td><b>Surname</td>
+<td><b>Category</td>
+<td><b>Date</td>
+<td><b>Amount</td>
+</tr>
+@foreach ($amounts as $bus)
+<tr style="text-align: center;">
+<td>{{ $bus->name }}</td>
+<td>{{ $bus->surname }}</td>
+<td>{{ $bus->category }}</td>
+<td>{{ $bus->created_at }}</td>
+<td>{{ $bus->amount }}</td>
+</tr>
+@endforeach
+</table>
+
+ Total: Tsh. <b>{{$schools}}</b>/=
                 </div>
             </div>
         </div>
